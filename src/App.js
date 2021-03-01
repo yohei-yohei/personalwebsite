c6b10cc2f2046653781579a724f332b134a29ef7
Sato <yoheisat@a483e7371006.ant.amazon.com>
2021-02-28 18:42:10 -0800

    change3

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>佐藤洋平</code> 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/yoheisato/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          About yohei sato
         </a>
       </header>
     </div>
