f2440fdafe5f8e10a86a381456269ca3025f9a1a
Sato <yoheisat@a483e7371006.ant.amazon.com>
2021-03-03 00:27:41 -0800

    icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from './yoheisato.jpg';
+import logo2 from './GitHub-Mark-64px.png';
+import logo3 from './iconfinder_1_Linkedin_unofficial_colored_svg_5296501.png';
 import './App.css';
 
 function App() {
@@ -9,14 +11,16 @@ function App() {
         <p>
           <code>佐藤洋平</code> 
         </p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/yoheisato/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          About yohei sato
+
+        <a href="https://www.linkedin.com/in/yoheisato/">
+        <img  src={logo3} className="LinkedInIcon" />
         </a>
+
+        <a href="https://github.com/yohei-yohei">
+        <img  src={logo2} className="githubIcon" />
+        </a>
+
+
       </header>
     </div>
   );
